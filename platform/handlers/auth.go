57091b164efbbcc11f78e4e6e3a89ecd0299849c
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-01-15 21:11:22 +0100

    Add missing docs and generate swagger

--- platform/handlers/auth.go
+++ platform/handlers/auth.go
@@ -4,15 +4,15 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// Login godoc
-// @Summary Login user
-// @Description Login user
+// AuthTokenFlow godoc
+// @Summary Authenticate user with email, password and issue access tokens
+// @Description Authenticate user with email, password and issue access tokens
 // @Tags auth
 // @Produce json
 // @Failure 403 {object} shared.HTTPError Invalid auth details
 // @Success 200 {object} schema.TokenResponse
 // @Router /auth/login [post]
-func (h *Handler) Login(ctx *fiber.Ctx) error {
+func (h *Handler) AuthTokenFlow(ctx *fiber.Ctx) error {
 	loginPayload, err := h.Params.LoginPayload(ctx)
 	if err != nil {
 		return err
@@ -49,15 +49,29 @@ func (h *Handler) RefreshToken(ctx *fiber.Ctx) error {
 // @Tags auth
 // @Produce json
 // @Success 204 {string} nil Log out is successful
-// @Router /auth/logout [get]
+// @Router /auth/logout [delete]
 func (h *Handler) LogOut(ctx *fiber.Ctx) error {
 	return h.AuthService.Logout(ctx)
 }
 
+// JWKS godoc
+// @Summary Returns jwks details
+// @Description Returns jwks details
+// @Tags auth
+// @Produce json
+// @Success 200 {object} nil
+// @Router /auth/jwks [get]
 func (h *Handler) JWKS(ctx *fiber.Ctx) error {
 	return ctx.JSON(h.JWXService.JWKS())
 }
 
+// Register godoc
+// @Summary Register using email and password
+// @Description Register using email and password
+// @Tags auth
+// @Produce json
+// @Success 204 {string} nil registration is successful
+// @Router /auth/register [post]
 func (h *Handler) Register(ctx *fiber.Ctx) error {
 	registerPayload, err := h.Params.RegisterPayload(ctx)
 	if err != nil {
