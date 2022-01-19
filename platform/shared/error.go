43b1283aee459703fe90f09f4cf2d58c0fd42e7e
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-01-19 23:33:45 +0100

    Cleanup redundant code

--- platform/shared/error.go
+++ platform/shared/error.go
@@ -11,41 +11,6 @@ type HTTPError struct {
 	Details interface{} `swaggertype:"object"`
 }
 
-func InvalidPayloadResponse(err error, code ErrorCode) HTTPError {
-	responseCode := InvalidPayload
-	if code == "" {
-		responseCode = code
-	}
-	return HTTPError{
-		Code:    responseCode,
-		Message: err.Error(),
-	}
-}
-
-func NotFoundResponse(msg string, code ErrorCode) HTTPError {
-	responseCode := NotFound
-	if code == "" {
-		responseCode = code
-	}
-
-	return HTTPError{
-		Code:    responseCode,
-		Message: msg,
-	}
-}
-
-func UnauthorizedResponse(msg string, code ErrorCode) HTTPError {
-	responseCode := Unauthorized
-	if code == "" {
-		responseCode = code
-	}
-
-	return HTTPError{
-		Code:    responseCode,
-		Message: msg,
-	}
-}
-
 func ServerErrorResponse(err error, code ErrorCode) HTTPError {
 	responseCode := ServerError
 	if code == "" {
