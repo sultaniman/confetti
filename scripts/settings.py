42ddc0e08af99a01dfa9cd5d68054f9c1f6c67db
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-05-28 17:11:08 +0200

    Test github action deploy

--- scripts/settings.py
+++ scripts/settings.py
@@ -9,4 +9,5 @@ client = session.client(
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
 )
-client.download_file('confetti', 'confetti-dev/confetti-key.pem', './file.pem')
+
+client.download_file('confetti', 'confetti-dev/confetti-dev.yaml', './confetti-dev.yaml')
